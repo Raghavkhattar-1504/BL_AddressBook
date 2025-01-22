5434efb5d70b5d748edd60dccb75a3cbbedf7ecd
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-22 16:50:32 +0530

    [Raghav] : ADD - Address Book UC1

--- AddressBook.js
+++ AddressBook.js
@@ -1,7 +1,32 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync"));
 class AddressBook {
     constructor() {
         this.arr = [];
     }
 }
+function addressBook() {
+    const address_book = new AddressBook();
+    const intro = ` Welcome to the Address Book
+    Enter the corresponding number to perform the operations:
+    0 -> Add Contacts
+    1 -> Delete Contact
+    2 -> Edit Contact
+    3 -> Get All Contacts
+    4 -> Exit `;
+    while (true) {
+        console.log(intro);
+        const input = parseInt(readline_sync_1.default.question("Enter a number"));
+        switch (input) {
+            case 0:
+                break;
+            case 1:
+                break;
+        }
+    }
+}
+addressBook();
--- AddressBook.ts
+++ AddressBook.ts
@@ -7,12 +7,66 @@ interface Contact {
     city : string,
     state : string,
     zip : number,
-    phonenumber : string,
+    phonenumber : number,
     email : string
 }
 
 class AddressBook {
     arr : Contact[] = [];
 
+    add(){
+
+        const firstname = readLineSync.question("Enter First Name");
+        const lastname = readLineSync.question("Enter Last Name");
+        const address = readLineSync.question("Enter Address");
+        const city = readLineSync.question("Enter City");
+        const state = readLineSync.question("Enter State");
+        const zip = parseInt(readLineSync.question("Enter Zip Code"));
+        const phonenumber = parseInt(readLineSync.question("Enter Phone Number"));
+        const email = readLineSync.question("Enter Email Address");
+
+        const user : Contact = {
+            firstname : firstname,
+            lastname : lastname,
+            address : address,
+            city : city,
+            state : state,
+            zip : zip,
+            phonenumber : phonenumber,
+            email : email
+        }
+        
+        this.arr.push(user);
+        console.log(this.arr);
+        
+    }
+}
+
+function addressBook() {
+    const address_book = new AddressBook();  
+
+    const intro : string = ` Welcome to the Address Book
+    Enter the corresponding number to perform the operations:
+    0 -> Add Contacts
+    1 -> Delete Contact
+    2 -> Edit Contact
+    3 -> Get All Contacts
+    4 -> Exit `
+
+    while (true) {
+        console.log(intro);
+
+        const input : number = parseInt(readLineSync.question("Enter a number"));
+        switch (input) {
+            case 0:
+                address_book.add();
+                break;
+        
+            default:
+                break;
+        }
+    }
     
 }
+
+addressBook();
